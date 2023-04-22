83894cd3c18a443eeb6754a486d3fc705c579769
christianhelms <helmsmedia@gmail.com>
2023-04-22 18:35:24 -0500

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lato: ['var(--font-lato)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
